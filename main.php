170fb8d7f7eedacd46c0f673d044c4c47aa04ede
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-12-09 16:44:40 +0000

    minor changes

--- main.php
+++ main.php
@@ -1,7 +1,9 @@
 <?php 
 include('header.php');
+include('list_events_popular.php');
+include('print_events.php');
 
-/*function display_register_form(){
+function display_register_form(){
 	
 	echo
     '<div id="register" title="Register form" style="display:none;"><form id="logNreg" action="log_in.php" method="post" enctype="multipart/form-data">
@@ -14,7 +16,8 @@ include('header.php');
         <br><input class="form_button" type="submit" value="REGISTER">
     </form></div>';
 
-}*/
+}
+
 
 function main_display()
 {
@@ -22,44 +25,59 @@ function main_display()
 	
 	switch($status)
 	{
-		case PHP_SESSION_DISABLED:
-			/*display_login_form();*/ 
-			break;
+
+		case PHP_SESSION_DISABLED: break; //display_login_form();
 		case PHP_SESSION_ACTIVE:
-			if(!checkLogged())
-			{
-				/*session_destroy();*/
-				display_register_form();
-			}
+			if(checkLogged()) ;
+			else { display_register_form();} //session_destroy();
 			break;
-		case PHP_SESSION_NONE:/*display_login_form();*/  break;
+		case PHP_SESSION_NONE:  break; //display_login_form();
 			default; break;
 	}
-}
+} 
 
 ?>
+
 
 <!DOCTYPE html>
 
 <html>
 
-	<header>
-		<title> RESTFeed LTW 2016/2017 </title>
-		<h1>RESTFeed</h1>
-		<meta charset = "utf-8">
-		<link rel="stylesheet" href="./css/myStyle.css">
-		<script type="text/javascript" src="main.js"></script>
-	</header>
+<header>
+<title> RESTFeed LTW 2016/2017 </title>
+<meta charset = "utf-8">
+<link rel="stylesheet" href="./css/myStyle.css">
+<?php meta_includes(); ?>
+<script type="text/javascript" src="main.js"></script>
+</header>
 
-	<body>
-		
-		<a href="list_events.php">Check all public events</a>
-		<footer>
-			<marquee behavior="scroll" direction="left">
-				Welcome to RESTFeed
-			</marquee>
-		</footer>
-		
-	</body>
+<body>
+
+    <header> 
+	<?php login_header(); ?>
+	<h1>RestFeed</h1> 
+	</header>
+	
+    <br>
+		<?php /*main_display()*/; print_events(popular_events(),true,"Popular Events");?>
+    <br>
+    <br>
+   <a href="list_events.php">Check all public events</a>
+	<br>
+	
+	<footer>
+        <marquee behavior="scroll" direction="left">
+            Welcome to RESTFeed
+        </marquee>
+
+       	<br>Date  
+	<div id="date_display"><?php // echo date('l jS \of F Y h:i:s A')?></div>
+<!-- echo date('l jS \of F Y h:i:s A'); 
+		<!--print_r(getdate()); -->
+	<!--echo date('Y-m-d H:i:s'); -->
+
+    </footer>
+	
+</body>
 
 </html>
