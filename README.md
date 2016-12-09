f6b62c1dd8b444bb8a30608a102a01768d6ef1e2
SonerSound <mario.shadic.esteves@hotmail.com>
2016-12-09 12:17:51 +0000

    Code cleanup, possible bugs

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 This is the github repo containing all data for the LTW project.
 
 ## Group members
+* [Mario Esteves](https://github.com/SonerSound) - 201607940
 * [Ricardo Silva](https://github.com/ricsilva93) - 201607942
--- main.php
+++ main.php
@@ -1,7 +1,5 @@
 <?php 
 include('header.php');
-include('list_events_popular.php');
-include('print_events.php');
 
 /*function display_register_form(){
 	
@@ -24,10 +22,15 @@ function main_display()
 	
 	switch($status)
 	{
-		case PHP_SESSION_DISABLED:/*display_login_form();*/ break;
+		case PHP_SESSION_DISABLED:
+			/*display_login_form();*/ 
+			break;
 		case PHP_SESSION_ACTIVE:
-			if(checkLogged()) ;
-			else {/*session_destroy();*/ display_register_form();}
+			if(!checkLogged())
+			{
+				/*session_destroy();*/
+				display_register_form();
+			}
 			break;
 		case PHP_SESSION_NONE:/*display_login_form();*/  break;
 			default; break;
@@ -40,41 +43,23 @@ function main_display()
 
 <html>
 
-<header>
-<title> RESTFeed LTW 2016/2017 </title>
-<meta charset = "utf-8">
-<link rel="stylesheet" href="./css/myStyle.css">
-<?php meta_includes(); ?>
-<script type="text/javascript" src="main.js"></script>
-</header>
-
-<body>
-
-    <header> 
-	<?php login_header(); ?>
-	<h1>RESTFeed</h1> 
+	<header>
+		<title> RESTFeed LTW 2016/2017 </title>
+		<h1>RESTFeed</h1>
+		<meta charset = "utf-8">
+		<link rel="stylesheet" href="./css/myStyle.css">
+		<script type="text/javascript" src="main.js"></script>
 	</header>
-	
-    <br>
-		<?php /*main_display()*/; print_events(popular_events(),true,"Popular Events");?>
-    <br>
-    <br>
-    <a href="list_events.php">Check all public events</a>
-	<br>
-	
-	<footer>
-        <marquee behavior="scroll" direction="left">
-            Welcome to RESTFeed
-        </marquee>
 
-       	<br>Date  
-	<div id="date_display"><?php // echo date('l jS \of F Y h:i:s A')?></div>
-<!-- echo date('l jS \of F Y h:i:s A'); 
-		<!--print_r(getdate()); -->
-	<!--echo date('Y-m-d H:i:s'); -->
-
-    </footer>
-	
-</body>
+	<body>
+		
+		<a href="list_events.php">Check all public events</a>
+		<footer>
+			<marquee behavior="scroll" direction="left">
+				Welcome to RESTFeed
+			</marquee>
+		</footer>
+		
+	</body>
 
 </html>
