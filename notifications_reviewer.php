616ab8f01d0edcbbf6f40d5e756b833abb9dc546
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-12-13 23:20:09 +0000

    new page & added new records to bd

---
+++ notifications_reviewer.php
@@ -0,0 +1,30 @@
+<?php 
+include('header.php');
+include('user_data.php');
+?>
+
+<html>
+  <head>
+    <title>Notifications    (EM OBRAS)</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/myStyle.css"> 
+	<?php meta_includes(); ?>
+  </head>
+  
+  <body>
+    <header>
+	<?php login_header(); ?>
+      <h1>Notifications   (EM OBRAS)</h1>
+    </header>
+	
+	<div class="left_float">
+	<h2>  </h2>
+	<br>notificações
+	
+	</div>
+	
+	
+	
+	</body>
+
+</html>
