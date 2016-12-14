c08c756bb5d48f283cf1fcae1649f4878a9072b1
SonerSound <mario.shadic.esteves@hotmail.com>
2016-12-14 15:38:35 +0000

    changes to db, and file structure
    php_sqlite_func holds all functions to manipulate the database

--- my_reviews.php
+++ my_reviews.php
@@ -1,30 +1,25 @@
 <?php 
-include('header.php');
-include('user_data.php');
+include_once('header.php');
+include_once('php_sqlite_func.php');
 ?>
 
 <html>
-  <head>
-    <title>My Reviews    (EM OBRAS)</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/myStyle.css"> 
-	<?php meta_includes(); ?>
-  </head>
-  
-  <body>
-    <header>
-	<?php login_header(); ?>
-      <h1>My Reviews    (EM OBRAS)</h1>
-    </header>
-	
-	<div class="left_float">
-	<h2>  </h2>
-	<br>AS MINHAS REVIEWS BLA BLA BLA 
-	<?php getUserReviews(); ?>
-	</div>
-	
-	
-	
+	<head>
+		<title>Reviews</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/myStyle.css"> 
+		<?php meta_includes(); ?>
+	</head>
+
+	<body>
+		<header>
+			<?php login_header(); ?>
+		</header>
+
+		<div class="left_float">
+			<h2>My Reviews</h2>
+			<?php getUserReviews(); ?>
+		</div>
 	</body>
 
 </html>
--- php_sqlite_func.php
+++ php_sqlite_func.php
@@ -1,5 +1,53 @@
 <?php
 
+include_once('header.php');
+
+/* User functions */
+
+function getUserReviews()
+{
+	if(checklogged())
+	{
+		//get all comments based on username
+		
+		$db = new PDO('sqlite:rest.db');
+
+
+		$stmt = $db->prepare("SELECT restaurante_reviews.stars as stars, restaurante_reviews.comentario as comentario, restaurante.nome as nome 
+							 FROM restaurante_reviews INNER JOIN restaurante 
+							 ON restaurante.id = restaurante_reviews.restaurant_id AND user_id = ?");
+
+
+		$stmt->execute(array($_SESSION['login_user']));
+
+		$results = $stmt->fetchAll();
+		$html_string = "<ul>";
+		
+		//go through all the results and build the html from it
+		foreach($results as &$review)
+		{
+			$html_string .= "<li>";
+			
+			$html_string .= "<h3> " . $review['nome'] . " </h3><br><br>" .
+							"<h4> " . $review['stars'] . " </h4><br>";
+			if($review['comentario'] != null)
+			{
+				$html_string .= "<h5> " . $review['comentario'] . "</h5>";
+			}
+			
+			$html_string .= "</li>";
+		}
+		
+		//show a list with all the scores, and comments from each review
+		
+		$html_string .= "</ul>";
+		
+		echo $html_string;
+	}
+}
+
+/* Restaurant functions */
+
 function getRestaurants($restName, $sortMode)
 {
 	$db = new PDO('sqlite:rest.db');
@@ -103,7 +151,7 @@ function printSearchRestaurants()
 	
 	$restaurantsList = getRestaurants($_GET['restName'], $_GET['sortMode']);
 	
-	$html_string = "<ul>";
+	$html_string = "Restaurant Reviews<br><ul>";
 	
 	foreach($restaurantsList as &$restaurant)
 	{
@@ -118,7 +166,7 @@ function printSearchRestaurants()
 	
 	$html_string .= "</ul>";
 	
-	if(strcmp($html_string, "<ul></ul>") == 0)
+	if(strcmp($html_string, "Restaurant Reviews<br><ul></ul>") == 0)
 	{
 		$html_string = "No restaurants found matching your criteria.";
 	}
@@ -134,6 +182,20 @@ function getRestaurantName($restId)
 	$results = $stmt->fetchAll();
 	
 	return $results[0]['nome'];
+}
+
+function insertNewReviewOnRestaurant($restId, $restStars, $restComment)
+{
+	$db = new PDO('sqlite:rest.db');
+	$stmt = $db->prepare("INSERT INTO restaurante_reviews VALUES (?, ?, ?, ?, ?)");
+	$stmt->execute(array($restStars, $restComment, $_SESSION['login_user'], $restId, null));
+}
+
+function deleteReviewFromRestaurant($revId)
+{
+	$db = new PDO('sqlite:rest.db');
+	$stmt = $db->prepare("DELETE FROM restaurant WHERE id = ?");
+	$stmt->execute(array($revId));
 }
 
 ?>
