10bbfa59c672c978c31c5e09a7ce471e297a19dd
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-12-14 19:15:34 +0000

    my restaurants almost done

--- My_Restaurants.php
+++ My_Restaurants.php
@@ -6,50 +6,45 @@ include_once('php_sqlite_func.php');
 <!DOCTYPE html>
 
 <html>
-<header>
-<title> New Restaurant </title>
-<meta charset = "utf-8">
-<link rel="stylesheet" href="./css/myStyle.css">
-<?php meta_includes(); ?>
-<script type="text/javascript" src="main.js"></script>
-</header>
+  <head>
+    <title>My Restaurants</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/myStyle.css"> 
+    <?php meta_includes(); ?>
+  </head>
 
-<body>
-
-    <header> 
-	<h1>RestFeed</h1> <br>
+  <body>
+    <header>
+      <h1>RestFeed</h1> <br>
   <?php login_header(); ?>
-	</header>
-	
-    <br>
+    </header>
 
-
-
-    <br>
-    <br>
-	My Restaurants
-
-	<br><br>
-	In this section, you can view your previously added restaurants.<br>
-	You can also edit a restaurant or delete it.
-
-	<br>
-	<br>
-
-
-    
-
+    <div class="left_float">
+      <h2>My Restaurants</h2>
+      <?php $result = getUserReviews();
+  $html_string = "<ul>";
   
-	<br>
-	
-	<footer>
-	
-       	<br>
-	<div id="date_display"><?php // echo date('l jS \of F Y h:i:s A')?></div>
-
-
-    </footer>
-	
-</body>
+  //go through all the results and build the html from it
+  foreach($result as &$restaurants)
+  {
+   $html_string .= "<li>";
+    
+    $html_string .= "<h3> " . $restaurants['nome'] . " </h3><br><br>" .
+            "<h4> " . $restaurants['descricao'] . " </h4><br>".
+            "<h5> " . $restaurants['num_reviews'] . " </h4><br>";
+   
+    
+    $html_string .= "</li>";
+  }
+  
+  //show a list with all the scores, and comments from each review
+  
+  $html_string .= "</ul>";
+  
+  echo $html_string; ?>
+    </div>
+  </body>
 
 </html>
+In this section, you can view your previously added restaurants.<br>
+  You can also edit a restaurant or delete it.
