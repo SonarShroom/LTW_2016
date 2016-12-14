bda9a70621eb5e3297d545289f905c59dc3b7201
Ricardo Silva <ricardoapsilva93@hotmail.com>
2016-12-14 00:14:54 +0000

    fixed bug in get reviews

--- user_data.php
+++ user_data.php
@@ -1,37 +1,65 @@
 <?php
 
+include_once('header.php');
+
 function getUserReviews()
 {
-	//get all comments based on username
+	//check if logged
 	
-	$db = new PDO('sqlite:rest.db');
-	$stmt = $db->prepare('SELECT restaurante_reviews.stars as stars, restaurante_reviews.comentario as comentario, restaurante.nome as nome ' .
-						 'FROM restaurante_reviews INNER JOIN restaurante ' .
-						 'ON restaurante.id = restaurante_reviews.restaurante_id AND user_id = ?');
-	$stmt->execute(array($_SESSION['login_user']));
-	$results = $stmt->fetchAll();
-	$html_string = "<ul>";
-	
-	//go through all the results and build the html from it
-	foreach($results as &$review)
+	if(checklogged())
 	{
-		$html_string .= "<li>";
+		//get all comments based on username
 		
-		$html_string .= "<h3> " . $review['nome'] . " </h3><br><br>" .
-						"<h4> " . $review['stars'] . " </h4><br>";
-		if($review['comentario'] != null)
+		$db = new PDO('sqlite:rest.db');
+
+
+		$stmt = $db->prepare("SELECT restaurante_reviews.stars as stars, restaurante_reviews.comentario as comentario, restaurante.nome as nome 
+							 FROM restaurante_reviews INNER JOIN restaurante 
+							 ON restaurante.id = restaurante_reviews.restaurant_id AND user_id = ?");
+
+
+		$stmt->execute(array($_SESSION['login_user']));
+
+		$results = $stmt->fetchAll();
+		$html_string = "<ul>";
+		
+		//go through all the results and build the html from it
+		foreach($results as &$review)
 		{
-			$html_string .= "<h5> " . $review['comentario'] . "</h5>";
+			$html_string .= "<li>";
+			
+			$html_string .= "<h3> " . $review['nome'] . " </h3><br><br>" .
+							"<h4> " . $review['stars'] . " </h4><br>";
+			if($review['comentario'] != null)
+			{
+				$html_string .= "<h5> " . $review['comentario'] . "</h5>";
+			}
+			
+			$html_string .= "</li>";
 		}
 		
-		$html_string .= "</li>";
+		//show a list with all the scores, and comments from each review
+		
+		$html_string .= "</ul>";
+		
+		echo $html_string;
 	}
 	
-	//show a list with all the scores, and comments from each review
-	
-	$html_string .= "</ul>";
-	
-	echo $html_string;
+}
+
+function deleteReviews(){
+$db = new PDO('sqlite:rest.db');
+		
+
+		$stmt = $db->prepare("SELECT restaurante_reviews.stars as stars, restaurante_reviews.comentario as comentario, restaurante.nome as nome 
+							 FROM restaurante_reviews INNER JOIN restaurante 
+							 ON restaurante.id = restaurante_reviews.restaurant_id AND user_id = ?");
+
+		DELETE 
+
+
+		$stmt->execute(array($_SESSION['login_user']));
+
 }
 
 ?>
